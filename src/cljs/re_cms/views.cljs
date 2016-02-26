e9b8f656e3fce3f06462e5e38358a29234b4dfaa
Piotr Jagielski <pjg@touk.pl>
2016-02-26 20:38:46 +0100

    reaction based changed detection

--- src/cljs/re_cms/views.cljs
+++ src/cljs/re_cms/views.cljs
@@ -4,37 +4,32 @@
             [re-frame.core :as re-frame]
             [markdown.core :as md]))
 
-(defn- reset-edit-mode [new changed]
-  (reset! changed false)
+(defn- reset-edit-mode [new]
   (reset! new nil))
 
-(defn- activate-edit-mode [new changed]
-  (reset! changed false)
-  (reset! new {:page "" :section "" :value ""}))
+(defn- activate-edit-mode [new]
+  (reset! new {:page nil :section nil :value nil}))
 
-(defn- select-component [key value-ref options new-row-ref changed]
+(defn- select-component [key value-ref options new-row-ref]
   [:div.col-md-6
    (if @new-row-ref
      [:input {:class     "form-control re-input"
               :type      "text" :value (get @new-row-ref key)
-              :on-change #(do (swap! new-row-ref assoc key (-> % .-target .-value))
-                              (reset! changed false))}]
+              :on-change #(swap! new-row-ref assoc key (-> % .-target .-value))}]
      [:select {:class     "form-control re-input"
                :value     @value-ref
-               :on-change #(do (reset! value-ref (-> % .-target .-value))
-                               (reset! changed false))}
+               :on-change #(reset! value-ref (-> % .-target .-value))}
       (for [option options]
         ^{:key option} [:option option])])])
 
-(defn- textarea-component [data preview changed]
+(defn- textarea-component [data preview]
   [:div.col-md-12
    [:textarea.re-editor
     {:cols      115
      :rows      10
      :value     (:value @data)
      :on-change #(do (swap! data assoc :value (-> % .-target .-value))
-                     (reset! preview (md/md->html (-> % .-target .-value)))
-                     (reset! changed true))}]])
+                     (reset! preview (md/md->html (-> % .-target .-value))))}]])
 
 (defn row-editor [data]
   (let [page (reaction (-> @data keys first))
@@ -43,33 +38,36 @@
         new-row (atom nil)
         preview (atom nil)
         show-preview (atom false)
-        changed (atom false)]
+        changed (reaction (not= (:value @row)
+                                (:value (-> @data (get @page) (get @section)))))
+        new-ok (reaction (not (or (nil? (:page @new-row))
+                                  (nil? (:section @new-row))
+                                  (nil? (:value @new-row)))))]
     (fn []
       [:div
        [:div.row
-        (select-component :page page (keys @data) new-row changed)
-        (select-component :section section (-> (get @data @page) keys) new-row changed)]
+        (select-component :page page (keys @data) new-row)
+        (select-component :section section (-> (get @data @page) keys) new-row)]
        [:div.row
-        (textarea-component (if @new-row new-row row) preview changed)]
+        (textarea-component (if @new-row new-row row) preview)]
        [:div.row
         (when @show-preview
           [:div {:dangerouslySetInnerHTML {:__html @preview}}])
         [:div.col-md-6
          (if @new-row
            [:button.btn.re-button
-            {:on-click #(reset-edit-mode new-row changed)} "Cancel"]
+            {:on-click #(reset-edit-mode new-row)} "Cancel"]
            [:button.btn.re-button
-            {:on-click #(activate-edit-mode new-row changed)} "New entry"])]
+            {:on-click #(activate-edit-mode new-row)} "New entry"])]
         [:div.col-md-6
          (if @new-row
            [:button.btn.re-button.action
-            {:class (when-not @changed "disabled")
+            {:class    (when-not @new-ok "disabled")
              :on-click #(do (re-frame/dispatch [:save @new-row])
-                            (reset-edit-mode new-row changed))} "Save"]
+                            (reset-edit-mode new-row))} "Save"]
            [:button.btn.re-button.action
             {:class    (when-not @changed "disabled")
-             :on-click #(do (re-frame/dispatch [:update @row])
-                            (reset! changed false))} "Update"])]]])))
+             :on-click #(re-frame/dispatch [:update @row])} "Update"])]]])))
 
 (defn app []
   (let [content (re-frame/subscribe [:content])]
